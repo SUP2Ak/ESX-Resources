6523eb347b7a2d8308a4332f09263c738cc1e30f
Wesley <cormier.wesley@gmail.com>
2021-05-12 07:08:52 +0200

    Ajout myServer v1.0

---
+++ myServer_setting/fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'myServerESX SETTING'
+
+version 'v1.0'
+
+client_scripts {
+	'cl_setting.lua',
+	'config.lua',
+}
+
+--[[					__SUP2Ak__                            
+    /Discord SUP2Ak FiveM-Dev : https://discord.gg/hYCR2YKgxB    /  
+    /GitHub : https://github.com/SUP2Ak	                        /   
+    /Youtube : https://www.youtube.com/SUPAAkHD                /
+    __________________________________________________________/
+    |                   __MyProject__
+    |    /Serveur RP : AlphaV RP
+ ___|    /FiveM      : myServerESX (qui est dans le liens Discord : FiveM-Dev)					  
+]]
